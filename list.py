6a284a3f8b8dbdbfb0ffe5a09cd483d00293b8de
Aishwarya Panse <aishwaryapanse3@gmail.com>
2025-12-25 19:13:31 +0530

    Added dict

--- list.py
+++ list.py
@@ -1,1 +1,15 @@
 students= [10,20,30,40,50,60,70,80,90,100]
+departments =[
+    {
+    id: 101,
+    "name" :"IT"
+},
+   {
+    id: 102,
+    "name" :"HR"
+},
+   {
+    id: 103,
+    "name" :"Accounts"
+}
+]
